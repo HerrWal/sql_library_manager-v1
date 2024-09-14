3c8f8c423213975b102ddb103f9ff9e05bb5de84
HerrWal <85037202+HerrWal@users.noreply.github.com>
2024-09-13 21:48:00 -0400

    Sep 13

--- project_files/app.js
+++ project_files/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
 
 const { sequelize } = require('./models/index');
 const { error } = require('console');
@@ -34,13 +34,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const error = createError(404, 'Page not found');
-  res.render('library/page_not_found', { error });
-});
+  res.render('page_not_found', { error });
+}); 
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  err.messsage = err.message || `Something went wrong with the server!`
+  err.message = err.message || `Something went wrong with the server!`
   console.log(`Error: ${res.status}, ${res.message}`);
   res.render('error', { err });
 });
